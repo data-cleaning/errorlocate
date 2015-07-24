eb7706c1e9eecdfa1b527a295cec3bd8906e32aa
Edwin de Jonge <edwindjonge@gmail.com>
2015-07-25 01:00:57 +0200

    wip

--- R/localize-errors.R
+++ R/localize-errors.R
@@ -2,3 +2,16 @@
 #' @export
 localize_errors <- function(data){
 }
+
+
+# test 1, 2, 3
+dat <- data.frame(x=1:2, y=3:2, a=c("C", "A"))
+
+v <- validator(
+  x > 1,
+  y > x,
+  a %in% c("A", "B")
+)
+
+confront(dat, v)
+
