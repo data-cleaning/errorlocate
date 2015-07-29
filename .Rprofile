0790dbfd7eb8311f1122836c37c71a16bf712dd9
Edwin de Jonge <edwindjonge@gmail.com>
2015-07-29 23:06:54 +0200

    tweak...

--- .Rprofile
+++
@@ -1,3 +0,0 @@
-if (!requireNamespace("validate", quietly = TRUE)){
-  utils::install.packages('validate', repos=c('http://cran.rstudio.com', 'http://data-cleaning.github.io/drat/'))
-}
--- validate.sh
+++
@@ -1,8 +0,0 @@
-#!bin/bash
-Rscript -e "install.packages(c('devtools', 'roxygen2'))"
-curl -L http://github.com/data-cleaning/validate/archive/master.tar.gz | tar xz
-cd validate-master
-bash ./build.sh
-R CMD INSTALL pkg
-cd ..
-rm -rf validate-master
