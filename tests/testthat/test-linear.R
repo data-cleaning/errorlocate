06663eaa7f68a98fec74634e834258e6ea68486c
Edwin de Jonge <edwindjonge@gmail.com>
2017-01-03 11:32:27 +0100

    updated tests for linear

--- tests/testthat/test-linear.R
+++ tests/testthat/test-linear.R
@@ -5,6 +5,10 @@ describe("is_lin_",{
     e <- quote(x>1)
     expect_true(is_lin_(e))
   })
+  it("can detect non-linear expressions",{
+    e <- quote(if(x>1) y < 1)
+    expect_false(is_lin_(e))
+  })
 })
 
 describe("is_linear",{
@@ -20,6 +24,22 @@ describe("lin_mip_rule",{
     mr <- lin_mip_rule_(e, name="H")
     expect_equal(mr$a, c(x=1))
     expect_equal(mr$b, 1)
+  })
+  it("handles unary operators",{
+    e <- quote(-x > 1)
+    mr <- lin_mip_rule_(e, name="H")
+    expect_equal(mr$a, c(x=-1))
+    expect_equal(mr$b, 1)
+  })
+  it("handles permutation of factors",{
+    e <- quote(x*2 > 1)
+    mr <- lin_mip_rule_(e, name="H")
+    expect_equal(mr$a, c(x=2))
+    expect_equal(mr$b, 1)
+  })
+  it("errors on invalid input", {
+    e <- quote(if (x < 1) y > 1)
+    expect_error(lin_mip_rule_(e))
   })
 })
 
