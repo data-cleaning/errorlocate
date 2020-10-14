49143ebb1d95803ae929f423e1338cd5e22dab96
Edwin de Jonge <edwindjonge@gmail.com>
2020-10-14 23:44:32 +0200

    added log_example

---
+++ issues/log_stuff.R
@@ -0,0 +1,27 @@
+rules <- validator( log10(prijs) == log10(m2prijs) + log10(oppervlakte)
+                  , prijs >= 1e5
+                  , oppervlakte >= 20
+                  )
+
+d <- data.frame( prijs = 2e5, oppervlakte = 20, m2prijs = 2000)
+
+mip <- miprules(rules)
+
+mip$._vars
+mip$._vars_num
+mip$._log_transform
+
+values <- as.list(d)
+weight <-  c(prijs = 1, m2prijs=5, oppervlakte = 5)
+log_values <- log_derived_data(values, mip$._log_transform)
+mip$set_values( values
+              , weights = weight
+              , log_values = log_values
+              , delta_names = mip$._log_transform$num_vars)
+mip$objective
+res <- mip$execute()
+res
+lpSolveAPI::write.lp(res$lp, filename = "test.lp")
+
+el <- locate_errors(d, rules, weight = weight)
+el$errors
