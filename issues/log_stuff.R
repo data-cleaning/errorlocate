98d114c56026d321e65005e7f285427be7f25d23
Edwin de Jonge <edwindjonge@gmail.com>
2020-10-15 09:11:31 +0200

    tweak

--- issues/log_stuff.R
+++ issues/log_stuff.R
@@ -5,6 +5,12 @@ rules <- validator( log10(prijs) == log10(m2prijs) + log10(oppervlakte)
 
 d <- data.frame( prijs = 2e5, oppervlakte = 20, m2prijs = 2000)
 
+el <- locate_errors(d, rules, weight = weight)
+el$errors
+
+
+### more detail:
+
 mip <- miprules(rules)
 
 mip$._vars
@@ -15,13 +21,11 @@ values <- as.list(d)
 weight <-  c(prijs = 1, m2prijs=5, oppervlakte = 5)
 log_values <- log_derived_data(values, mip$._log_transform)
 mip$set_values( values
-              , weights = weight
-              , log_values = log_values
-              , delta_names = mip$._log_transform$num_vars)
+                , weights = weight
+                , log_values = log_values
+                , delta_names = mip$._log_transform$num_vars)
 mip$objective
 res <- mip$execute()
 res
 lpSolveAPI::write.lp(res$lp, filename = "test.lp")
 
-el <- locate_errors(d, rules, weight = weight)
-el$errors
