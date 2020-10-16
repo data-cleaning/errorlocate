d4db896343d255cc6774ba5e4e465229cd57de22
Edwin de Jonge <edwindjonge@gmail.com>
2020-10-16 13:33:00 +0200

    added upper and lowerbound for log

--- R/log_extract.R
+++ R/log_extract.R
@@ -23,6 +23,49 @@ log_derived_data <- function(data, x){
   as.data.frame(derived_data)
 }
 
+log_constraint_rules <- function(num_var, log_var, logfn, n = 10, r = 1:1e5){
+  # TODO check that r[1] >= 1
+  r[1] <- max(r[1], 1)
+
+  # sample points based on slope
+  value_log <- seq(log(r[1]), log(r[2]), length.out = n)
+  value <- exp(value_log)
+
+  # TODO value_log again (to cope with that logfn can be different from "log")
+  # upper bound!
+  upper <-
+    lapply(seq_len(n), function(i){
+    a <- c(1, -1/value[i])
+    names(a) <- c(log_var, num_var)
+    b <- value_log[i] - 1
+    rule <- paste0(log_var, ".upperbound_",i)
+    mip_rule(a, "<=", b, rule = rule)
+  })
+
+  # lowerbound, more nasty...
+  lower <- list()
+  d_x <- diff(value)
+  d_y <- diff(value_log)
+
+  as <- d_y / d_x # check if d_x is zero?
+  bs <- value_log[-n] - as * value[-n]
+
+  lower <- structure(
+    lapply(seq_len(n-1), function(i){
+      substitute( log_var >= a * num_var + b
+                , list( a = as[i]
+                      , b=bs[i]
+                      , log_var = as.symbol(log_var)
+                      , num_var = as.symbol(num_var)
+                      )
+                )
+    }), class="dnf"
+  )
+  lower <- dnf_to_mip_rule(lower, name = paste0(log_var, ".lowerbound"))
+
+  c(upper, lower)
+}
+
 # TODO add to  tests
 # rules <- validator(log(x) > log(2))
 # data <- data.frame(x = 1:3)
